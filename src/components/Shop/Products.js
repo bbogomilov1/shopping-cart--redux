3ff0ef5296af3889a98d5b6bf716e90ae35ec439
bbogomilov1 <bdbogomilov@gmail.com>
2023-02-26 16:08:02 +0200

    managing cart items

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,37 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_ITEMS = [
+  {
+    id: "p1",
+    price: 6,
+    name: "My first book",
+    description: "This is my first book I've ever written",
+  },
+  {
+    id: "p2",
+    price: 2,
+    name: "My second book",
+    description: "This is my second book I've ever written",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_ITEMS.map((item) => {
+          return (
+            <ProductItem
+              key={item.id}
+              id={item.id}
+              title={item.name}
+              price={item.price}
+              description={item.description}
+            />
+          );
+        })}
       </ul>
     </section>
   );
